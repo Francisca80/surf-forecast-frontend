110bad209e4becc2f803d1666f23bf9a27ffbcf8
dashakrolik <45757824+dashakrolik@users.noreply.github.com>
2020-01-23 14:47:26 +0100

    add css sheet, add tueqouise box shadow

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactMapGL, { GeolocateControl } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css'
+import './Map.css';
 
 export class Map extends Component {
   state = {
@@ -8,22 +9,24 @@ export class Map extends Component {
   }
 
   render() {
-    const { viewport } = this.state;
-    
+    const { viewport } = this.state; 
+
     return (
 
-      <ReactMapGL {...viewport}
-        width="100vw"
-        height="100vh"
-        mapStyle="mapbox://styles/mapbox/outdoors-v11"
-        mapboxApiAccessToken={"pk.eyJ1IjoiZnJhbmNpc2NhODB2bSIsImEiOiJjazVjZnp3eTExNmowM2xwa3pieGJ1cDRpIn0.dXkJ6CzxbkcrqWK0Ij7xYA"}
+      <div className="map-wrapper">
+        <ReactMapGL {...viewport}
+          width="50vw"
+          height="50vh"
+          mapStyle="mapbox://styles/mapbox/outdoors-v11"
+          mapboxApiAccessToken={"pk.eyJ1IjoiZnJhbmNpc2NhODB2bSIsImEiOiJjazVjZnp3eTExNmowM2xwa3pieGJ1cDRpIn0.dXkJ6CzxbkcrqWK0Ij7xYA"}
 
-        onViewportChange={viewport => this.setState({ viewport })}>
-        <GeolocateControl
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-        />
-      </ReactMapGL>
+          onViewportChange={viewport => this.setState({ viewport })}>
+          <GeolocateControl
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+          />
+        </ReactMapGL>
+      </div>
     );
   }
 
