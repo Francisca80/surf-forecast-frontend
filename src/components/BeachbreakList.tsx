5de2519d8288412848d14c293aa8cdab0cd471e1
dashakrolik <45757824+dashakrolik@users.noreply.github.com>
2020-01-29 15:59:40 +0100

    make styled component from Material UI list, delete container

--- src/components/BeachbreakList.tsx
+++ src/components/BeachbreakList.tsx
@@ -2,37 +2,36 @@ import React, { FunctionComponent } from "react";
 import { Beachbreak } from "../models/beachbreak";
 import { BeachbreakListItem } from "./BeachbreakListItem";
 import List from '@material-ui/core/List';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 
 interface Props {
     beachbreaks: Beachbreak[];
     onDelete: (beachbreaks: Beachbreak) => void;
 }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 550,
-    backgroundColor: theme.palette.background.paper,
-    margin: "0 auto",
-    marginBottom: "30px",
-    marginTop: "20px"
-  },
-}));
-
-
+const StyledList = withStyles({
+    root: {
+        width: '100%',
+        maxWidth: 550,
+        backgroundColor: "white",
+        margin: "0 auto",
+        marginBottom: "30px",
+        marginTop: "20px"
+      },
+      padding: {
+        paddingTop: 0,
+        paddingBottom: 0
+      }
+})(List);
 
 export const BeachbreakList: FunctionComponent<Props> = ({ beachbreaks, onDelete }) => {
-    const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-        <List>
+        <StyledList>
             {beachbreaks.map(beachbreak => (
                 <BeachbreakListItem key={beachbreak.id} beachbreak={beachbreak} onDelete={onDelete} />
-                
+
             ))}
-        </List>
-        </div>
+        </StyledList>
     );
 };
