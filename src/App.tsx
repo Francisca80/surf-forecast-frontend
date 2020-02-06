274c0e07e3762d28a01052ca00931403787027d8
dashakrolik <45757824+dashakrolik@users.noreply.github.com>
2020-02-06 13:31:56 +0100

    commment back in

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,8 @@ class App extends Component<{}, State> {
   };
 
   componentDidMount() {
-    // this.getAllBeaches();
-    this.mockTimeOut();
+    this.getAllBeaches();
+    // this.mockTimeOut();
   }
 
   mockTimeOut = () => {
