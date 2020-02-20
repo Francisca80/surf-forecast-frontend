237c636d766d0b6cd4b9ba27c656bb313311e623
dashakrolik <45757824+dashakrolik@users.noreply.github.com>
2020-02-20 11:09:54 +0100

    change url

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
 export const baseUrl = "https://calm-badlands-88021.herokuapp.com/beaches";
-export const baseDevUrl = "http://localhost:3000/beaches";
+export const baseDevUrl = "http://localhost:8080/beaches";
 export const windyUrl = "https://api.windy.com/api/webcams/v2";
 export const locationIqUrl = "https://eu1.locationiq.com/v1/search.php?"
 export const mapboxToken = "pk.eyJ1IjoiZnJhbmNpc2NhODB2bSIsImEiOiJjazVjZnp3eTExNmowM2xwa3pieGJ1cDRpIn0.dXkJ6CzxbkcrqWK0Ij7xYA"
